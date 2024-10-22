78bc12079e1621f589fc37b8c9b44d41b583c1c5
Rian Brooks Flynn <112733140+rianbrooksflynn@users.noreply.github.com>
2024-10-22 12:29:30 -0400

    labeled the layernorm loops

--- hls4ml_projects/pytorch_layernorm_Vivado/build_lib.sh
+++ hls4ml_projects/pytorch_layernorm_Vivado/build_lib.sh
@@ -10,7 +10,7 @@ fi
 LDFLAGS=
 INCFLAGS="-Ifirmware/ap_types/"
 PROJECT=myproject
-LIB_STAMP=1eBf1F34
+LIB_STAMP=2bCAEA13
 WEIGHTS_DIR="\"weights\""
 
 ${CC} ${CFLAGS} ${INCFLAGS} -D WEIGHTS_DIR=${WEIGHTS_DIR} -c firmware/${PROJECT}.cpp -o ${PROJECT}.o
