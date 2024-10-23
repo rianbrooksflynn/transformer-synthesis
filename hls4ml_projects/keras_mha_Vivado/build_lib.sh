7d6246d6a113151311f59b1d4a89905d56267352
Rian Brooks Flynn <112733140+rianbrooksflynn@users.noreply.github.com>
2024-10-23 16:30:12 -0400

    more print statements

--- hls4ml_projects/keras_mha_Vivado/build_lib.sh
+++ hls4ml_projects/keras_mha_Vivado/build_lib.sh
@@ -10,7 +10,7 @@ fi
 LDFLAGS=
 INCFLAGS="-Ifirmware/ap_types/"
 PROJECT=myproject
-LIB_STAMP=308B8FB7
+LIB_STAMP=7f54cAA7
 WEIGHTS_DIR="\"weights\""
 
 ${CC} ${CFLAGS} ${INCFLAGS} -D WEIGHTS_DIR=${WEIGHTS_DIR} -c firmware/${PROJECT}.cpp -o ${PROJECT}.o
