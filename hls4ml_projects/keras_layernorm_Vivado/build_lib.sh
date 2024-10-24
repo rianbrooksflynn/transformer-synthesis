bcda35fbd594471b4010740d3b942959a2d3bd84
Rian Brooks Flynn <112733140+rianbrooksflynn@users.noreply.github.com>
2024-10-24 17:27:17 -0400

    fix some pragmas

--- hls4ml_projects/keras_layernorm_Vivado/build_lib.sh
+++ hls4ml_projects/keras_layernorm_Vivado/build_lib.sh
@@ -10,7 +10,7 @@ fi
 LDFLAGS=
 INCFLAGS="-Ifirmware/ap_types/"
 PROJECT=myproject
-LIB_STAMP=5eA15Cae
+LIB_STAMP=9F42eFc3
 WEIGHTS_DIR="\"weights\""
 
 ${CC} ${CFLAGS} ${INCFLAGS} -D WEIGHTS_DIR=${WEIGHTS_DIR} -c firmware/${PROJECT}.cpp -o ${PROJECT}.o
--- hls4ml_projects/pytorch_layernorm_Vivado/build_lib.sh
+++ hls4ml_projects/pytorch_layernorm_Vivado/build_lib.sh
@@ -10,7 +10,7 @@ fi
 LDFLAGS=
 INCFLAGS="-Ifirmware/ap_types/"
 PROJECT=myproject
-LIB_STAMP=4c9AEc6E
+LIB_STAMP=D27E2ddc
 WEIGHTS_DIR="\"weights\""
 
 ${CC} ${CFLAGS} ${INCFLAGS} -D WEIGHTS_DIR=${WEIGHTS_DIR} -c firmware/${PROJECT}.cpp -o ${PROJECT}.o
